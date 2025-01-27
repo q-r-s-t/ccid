33fd31bc3f529985ccade8a362ba8e502d7469f5
장은아 <aa@Eunas-MacBookPro.local>
2025-01-27 18:12:34 +0900

    Feat:header logo a tag

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -2,14 +2,18 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="logo-container p-6 fixed sm:hidden">
-      <Image
-        src="/img/cidc_logo.svg"
-        alt="CIDC Logo"
-        width={30}
-        height={30}
-        priority
-      />
-    </div>
+    <a href="#"
+    onClick={(e) => {
+      e.preventDefault(); // 기본 동작 차단
+      window.scrollTo({ top: 0, behavior: "smooth" }); // 상단으로 이동
+    }} className="logo-container p-6 fixed sm:hidden block">
+        <Image
+          src="/img/cidc_logo.svg"
+          alt="CIDC Logo"
+          width={30}
+          height={30}
+          priority
+        />
+    </a>
   );
 }
--- src/app/components/navmobile.js
+++ src/app/components/navmobile.js
@@ -23,17 +23,17 @@ export default function Navmobile({ textColor }) {
         onClick={toggleMenu}
       >
         <div
-          className={`w-5 border-t-1 transition-all duration-300 transform origin-top-left ${
+          className={`w-5 border-t-2 transition-all duration-300 transform origin-top-left ${
             isOpen ? "rotate-45" : ""
           }`}
         ></div>
         <div
-          className={`w-5 border-t-1 transition-all duration-300 transform ${
+          className={`w-5 border-t-2 transition-all duration-300 transform ${
             isOpen ? "opacity-0" : ""
           }`}
         ></div>
         <div
-          className={`w-5 border-t-1 transition-all duration-300 transform origin-bottom-left ${
+          className={`w-5 border-t-2 transition-all duration-300 transform origin-bottom-left ${
             isOpen ? "-rotate-45" : ""
           }`}
         ></div>
@@ -44,7 +44,7 @@ export default function Navmobile({ textColor }) {
           transition duration-700 ease-in-out ${
             isOpen
               ? "opacity-100 bg-[rgba(0,0,0,0.5)] backdrop-blur w-full h-full"
-              : "bg-[rgba(0,0,0,0)] opacity-0"
+              : "hidden opacity-0"
           }`}
       >
         <li>
