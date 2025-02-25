a5b7e05fabc1cbdf8cf7553c68b95c596a19bc09
장은아 <aa@Eunas-MacBookPro.local>
2025-02-25 21:52:25 +0900

    font route

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,21 +1,21 @@
 import localFont from "next/font/local";
 
 export const programme = localFont({
-  src: "../Programme-Light.woff",
+  src: "./Programme-Light.woff",
   weight: "400",
   style: "normal",
   display: "swap",
 });
 
 export const pxGrotesk = localFont({
-  src: '../fonts/Px-Grotesk-Regular.woff', // 경로 확인 (src 내부에서 접근)
+  src: './fonts/Px-Grotesk-Regular.woff', // 경로 확인 (src 내부에서 접근)
   weight: '400',
   style: 'normal',
   display: 'swap',
 });
 
 export const pretendardB = localFont({
-  src: '../fonts/Pretendard-Bold.subset.woff2', // 경로 확인 (src 내부에서 접근)
+  src: './fonts/Pretendard-Bold.subset.woff2', // 경로 확인 (src 내부에서 접근)
   weight: '800',
   style: 'normal',
   display: 'swap',
