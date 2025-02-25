08992b8ccc9d55b9140f7b2a0f6696f7f802948e
장은아 <aa@Eunas-MacBookPro.local>
2025-02-25 22:54:01 +0900

    members name pretendardB kr

--- src/fonts/dist/fonts.dev.js
+++ src/fonts/dist/fonts.dev.js
@@ -3,14 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.pxGrotesk = exports.programme = void 0;
+exports.pretendardB = exports.pxGrotesk = exports.programme = void 0;
 
 var _local = _interopRequireDefault(require("next/font/local"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var programme = (0, _local["default"])({
-  src: "./Programme-Light.woff",
+  src: "../fonts/Programme-Light.woff",
   weight: "400",
   style: "normal",
   display: "swap"
@@ -24,3 +24,11 @@ var pxGrotesk = (0, _local["default"])({
   display: 'swap'
 });
 exports.pxGrotesk = pxGrotesk;
+var pretendardB = (0, _local["default"])({
+  src: '../fonts/Pretendard-Bold.subset.woff',
+  // 경로 확인 (src 내부에서 접근)
+  weight: '400',
+  style: 'normal',
+  display: 'swap'
+});
+exports.pretendardB = pretendardB;
--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const programme = localFont({
-  src: "./Programme-Light.woff",
+  src: "../fonts/Programme-Light.woff",
   weight: "400",
   style: "normal",
   display: "swap",
@@ -9,6 +9,13 @@ export const programme = localFont({
 
 export const pxGrotesk = localFont({
   src: '../fonts/Px-Grotesk-Regular.woff', // 경로 확인 (src 내부에서 접근)
+  weight: '400',
+  style: 'normal',
+  display: 'swap',
+});
+
+export const pretendardB = localFont({
+  src: '../fonts/Pretendard-Bold.subset.woff', // 경로 확인 (src 내부에서 접근)
   weight: '400',
   style: 'normal',
   display: 'swap',
