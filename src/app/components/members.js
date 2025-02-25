191aeb1a1c06235932b15afd33bcdc74f35c05cd
장은아 <aa@Eunas-MacBookPro.local>
2025-02-25 23:12:07 +0900

    Feat member typography

--- src/app/components/members.js
+++ src/app/components/members.js
@@ -151,7 +151,7 @@ export default function Members() {
               <p className="leading-[1.5] text-[3.2vw] md:text-[2.1vw] lg:text-[1vw]">{member[3]}</p>
               <div className="border-l border-current mt-6 md:mt-4 pl-3 lg:pl-[0.5vw]">
                 <h3 className="pb-3 md:pb-2 leading-[1.3] text-[3vw] md:text-[1.9vw] lg:text-[0.8vw]">Career</h3>
-                <div className="leading-[1.3] text-[3vw] md:text-[1.7vw] lg:text-[0.7vw]">
+                <div className="leading-[1.45] text-[3vw] md:text-[1.7vw] lg:text-[0.7vw]">
                   <pre className={`whitespace-pre-wrap ${pxGrotesk.className}`}>{member[4]}</pre>
                 </div>
               </div>
@@ -177,7 +177,7 @@ export default function Members() {
                 />
                 <div className="flex flex-col justify-center pr-4 lg:pt-0 lg:pl-2">
                   <h3 className={`${pretendardB.className} leading-relaxed lg:leading-none text-[3vw] md:text-[1.9vw] lg:text-[0.9vw] xl:mb-[0.4vh] `}>{member[1]}</h3>
-                  <h2 className="leading-none text-[2.6vw] md:text-[1.6vw] lg:text-[0.75vw]">{member[3]}</h2>
+                  <h2 className="leading-none text-[3.1vw] md:text-[1.6vw] lg:text-[0.75vw]">{member[3]}</h2>
                   <pre className={` pt-[0.5vh] xl:pt-0 whitespace-pre-wrap leading-tight text-[2.4vw] md:text-[1.3vw] lg:text-[0.65vw] xl:mt-[0.5vh] ${pxGrotesk.className}`}>{member[4]}</pre>
                 </div>
               </li>
