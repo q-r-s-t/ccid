5c08068ad1876315d0ae1d75705b93a9c9fe987b
장은아 <aa@Eunas-MacBookPro.local>
2025-02-25 18:20:28 +0900

    Feat: EN/KR mobile pos

--- src/app/components/nav/lang.js
+++ src/app/components/nav/lang.js
@@ -4,7 +4,7 @@ import { programme } from "@/fonts/fonts";
 export default function LanguageToggle({textColor}){
     const {lang, toggleLang} = useLanguageStore();
     return(
-        <button onClick={toggleLang} className={`${programme.className} lg:font-[300] leading-relaxed text-xl font-normal lg:text-[0.9vw] z-[300] w-fit fixed bottom-0 left-0 pl-4 pb-10 lg:pb-0 lg:top-0 lg:left-auto lg:right-0 lg:pt-2 lg:pr-10`} style={{color : textColor}}>
+        <button onClick={toggleLang} className={`${programme.className} lg:font-[300] leading-relaxed text-xl font-normal lg:text-[0.9vw] z-[300] w-fit fixed bottom-0 left-0 pl-6 pb-10 lg:pb-0 lg:top-0 lg:left-auto lg:right-0 lg:pt-2 lg:pr-10`} style={{color : textColor}}>
            EN / KR
         </button>
     );
