081f5538a76ad196839784354ca0b7d9cf72fd83
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-04 13:51:24 +0900

    Update members.js

--- src/app/components/members.js
+++ src/app/components/members.js
@@ -100,7 +100,7 @@ export default function Members() {
         const res = await fetch(
           `${
             process.env.NODE_ENV === "production"
-              ? "https://cidc.vercel.app"
+              ? "https://c-c-i-d.vercel.app"
               : ""
           }/api/sheets`
         );
