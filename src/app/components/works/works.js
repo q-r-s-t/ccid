032909b9fffa518e2adc4783cf53fcf76ae6762d
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-10 23:45:51 +0900

    Update works.js

--- src/app/components/works/works.js
+++ src/app/components/works/works.js
@@ -42,7 +42,7 @@ export default function Works({ textColor }) {
                   return (
                     <div key={`${i}-${j}`} className="relative">
                       <div
-                        className={` ${lang === 'en' ? 'leading-[1.4] mb-[0.5vh]' : 'lg:leading-[1.8] leading-[1.6] mb-[0.9vh] lg:mb-[0.2vh]'} relative flex lg:hover:text-[#ff0] text-[3vw] md:text-[2.4vw] lg:text-[1vw]  
+                        className={` ${lang === 'en' ? 'leading-[1.4] mb-[0.5vh]' : 'lg:leading-[1.8] leading-[1.6] mb-[0.9vh] lg:mb-[0.2vh]'} relative flex text-[3vw] md:text-[2.4vw] lg:text-[1vw]  
                         ${j === 0 ? `text-primaryC lg:pl-10 lg:h-[7dvh] lg:absolute lg:top-0 pl-6 lg:left-0 lg:w-[20vw]` : ""}`}
                       >
                         {/* 년도 */}
