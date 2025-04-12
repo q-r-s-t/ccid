a9e3cc271805b22de8bb5c9fbccbe04339bdd1d8
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-13 00:19:32 +0900

    Update works.js

--- src/app/components/works/works.js
+++ src/app/components/works/works.js
@@ -49,7 +49,7 @@ export default function Works({ textColor }) {
                         <div
                           className={`${j === 1 ? `${lang === 'en' ? 'lg:mt-[0vh]' : 'lg:mt-[2vh]'}` : ''} ${
                             j === 0 ? "hidden" : ""
-                          }  font-dmmono flex-[0.35] lg:pl-[22.5%] lg:text-[1.1vw] lg:flex-[0.1] pl-6 lg:pl-12`}
+                          }  font-dmmono flex-[0.35] lg:pl-[22.5%] lg:text-[1vw] lg:flex-[0.1] pl-6 lg:pl-12`}
                         >
                           {year}
                         </div>
