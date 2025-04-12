bfe372dc2ab25c097af94aa667986ecdfb7d3f54
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-13 00:44:05 +0900

    Update contact.js

--- src/app/components/contact/contact.js
+++ src/app/components/contact/contact.js
@@ -1,6 +1,8 @@
+
 "use client";
 import { useEffect, useState } from "react";
 import "./contact.css";
+
 export default function Contact({ borderRadius, sectionOn }) {
   const [aboutInfo, setAboutInfo] = useState(null);
 
@@ -56,8 +58,7 @@ export default function Contact({ borderRadius, sectionOn }) {
 
   return (
     <div
-      // style={{ borderRadius: `${borderRadius}px` }}
-      className={`transition-all duration-[1000ms] ease-out absolute top-0 left-0 w-full h-full gradient-bg`}
+      className={\`transition-all duration-[1000ms] ease-out absolute top-0 left-0 w-full h-full gradient-bg\`}
     >
       <svg xmlns="http://www.w3.org/2000/svg">
         <defs>
@@ -80,33 +81,30 @@ export default function Contact({ borderRadius, sectionOn }) {
         <div className="interactive"></div>
       </div>
 
-      {/* purple tone layer */}
       <div
         className="bg-[rgba(164,106,251,0.3)] absolute top-0 left-0 w-full h-full"
         style={{ mixBlendMode: "darker" }}
       ></div>
 
-      {/* gradient layer */}
       <div
-        className={` ${sectionOn === 'contact' ? 'opacity-0' : 'opacity-100'} transition-opacity delay-[0.5s] duration-[1s] ease-in-out bg-gradient-to-b from-primaryW via-[rgba(93,0,156,0)] via-[rgba(93,0,156,0)] to-[rgba(93,0,156,0)]  absolute top-0 left-0 w-full h-full`}
+        className={\`\${sectionOn === 'contact' ? 'opacity-0' : 'opacity-100'} transition-opacity delay-[0.5s] duration-[1s] ease-in-out bg-gradient-to-b from-primaryW via-[rgba(93,0,156,0)] via-[rgba(93,0,156,0)] to-[rgba(93,0,156,0)]  absolute top-0 left-0 w-full h-full\`}
       ></div>
 
-      {/* 텍스트 */}
-      <div className="text-primaryB absolute w-full h-full top-0 left-0 flex flex-col justify-center text-center font-[400] leading-[1.8] text-[3.5vw] md:text-[3vw] lg:text-[1.05vw]">
+      <div className="text-black absolute w-full h-full top-0 left-0 flex flex-col justify-center text-center font-[400] leading-[1.8] text-[3.5vw] md:text-[3vw] lg:text-[1.05vw]">
         <p className="leading-snug z-[1]">
           For business inquiries please contact:
         </p>
 
         <div className="z-[1]">
           <a
-          href={`mailto:${aboutInfo?.[0]?.[2] || ""}`}
-          className="leading-snug pb-1 relative group text-primaryB hover:text-[#fff] transition-colors duration-1800"
-        >
-          {aboutInfo?.[0]?.[2] || "Loading..."}
-          <span
-            className="absolute bottom-0 left-0 w-0 h-0.5 bg-primaryB transition-all duration-[880ms] ease-in-out group-hover:w-full group-hover:bg-[#fff]"
-          ></span>
-        </a>
+            href={\`mailto:\${aboutInfo?.[0]?.[2] || ""}\`}
+            className="leading-snug pb-1 relative group text-black hover:text-white transition-colors duration-[1800ms]"
+          >
+            {aboutInfo?.[0]?.[2] || "Loading..."}
+            <span
+              className="absolute bottom-0 left-0 w-0 h-0.5 bg-black transition-all duration-[880ms] ease-in-out group-hover:w-full group-hover:bg-white"
+            ></span>
+          </a>
         </div>
       </div>
     </div>
