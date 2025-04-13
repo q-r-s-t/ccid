f3e542bcabc132a3633cf8c21d30aa40efb055ff
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-13 21:02:20 +0900

    Update desc.js

--- src/app/components/about/desc.js
+++ src/app/components/about/desc.js
@@ -13,8 +13,8 @@ function DescItem({ id, title, description, imageUrl }) {
 
   return (
     <li className="h-auto bg-gradient-to-t from-[rgba(93,0,156,0.2)] via-[rgba(93,0,156,0)] to-[rgba(93,0,156,0)] lg:flex group overflow-hidden transition-all duration-700 ease-out py-2 lg:py-[2vh] lg:px-[5vw]">
-      <h3
-        className={`${lang === 'en' ? textStyle : textStyleKr} ${programme.className} transition-all duration-700 w-full lg:w-[52%] tracking-[-0.02em] pt-[2px] lg:pr-[1vw] mb-[2vh]`}
+      <h3  
+        className={`${lang === 'en' ? textStyle : textStyleKr} ${neuehaas.className} transition-all duration-700 w-full lg:w-[52%] tracking-[-0.02em] pt-[2px] lg:pr-[1vw] mb-[2vh]`}
       >
         {title}
       </h3>
