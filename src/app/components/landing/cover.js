01e8dea4ffb76db66eefefd9aeb767b31cb901e4
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-14 01:39:00 +0900

    Update cover.js

--- src/app/components/landing/cover.js
+++ src/app/components/landing/cover.js
@@ -58,7 +58,7 @@ export default function Cover() {
   }, [currentWordIndex, mainText]);
 
   return (
-    <div className={`${programme.className} flex flex-col w-full h-full lg:pt-[38dvh] pt-[28vh] px-6 lg:px-10`}>
+    <div className={`flex flex-col w-full h-full lg:pt-[38dvh] pt-[28vh] px-6 lg:px-10`}>
       {typedWords.map((word, index) => {
         const shouldApplyColor = index <= currentWordIndex;
         const colorStyle = shouldApplyColor
@@ -71,10 +71,10 @@ export default function Cover() {
         return (
           <div
             key={index}
-            className="text-center program_set lg:text-left relative inline-block w-full lg:h-[24vw] h-[26vw] lg:h-[3.8vw] leading-[1.1] lg:leading-[1.3] text-[7.5vw] lg:text-[3vw]"
+            className="text-center lg:text-left relative inline-block w-full lg:h-[24vw] h-[26vw] lg:h-[3.8vw] leading-[1.1] lg:leading-[1.3] text-[7.5vw] lg:text-[3vw]"
           >
             <pre
-              className={`text-black lg:hidden whitespace-pre-wrap overflow-hidden relative ${
+              className={`${programme.className} program_set text-black lg:hidden whitespace-pre-wrap overflow-hidden relative ${
                 index === currentWordIndex ? "after:content-['|'] after:animate-blink" : ""
               }`}
               style={colorStyle}
@@ -82,7 +82,7 @@ export default function Cover() {
               {word}
             </pre>
             <p
-              className={`text-black hidden lg:block overflow-hidden relative ${
+              className={`${programme.className} program_set text-black hidden lg:block overflow-hidden relative ${
                 index === currentWordIndex ? "after:content-['|'] after:animate-blink" : ""
               }`}
               style={colorStyle}
