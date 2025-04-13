c3f09a93ee9df4e5ebb238f8961a6dee8b6afc9a
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-13 23:18:05 +0900

    Update cover.js

--- src/app/components/landing/cover.js
+++ src/app/components/landing/cover.js
@@ -71,7 +71,7 @@ export default function Cover() {
         return (
           <div
             key={index}
-            className="text-center lg:text-left relative inline-block w-full lg:h-[24vw] h-[26vw] lg:h-[3.8vw] leading-[1.1] lg:leading-[1.3] text-[7.5vw] lg:text-[38px]"
+            className="text-center lg:text-left relative inline-block w-full lg:h-[24vw] h-[26vw] lg:h-[3.8vw] leading-[1.1] lg:leading-[1.3] text-[7.5vw] lg:text-[2.5vw]"
           >
             <pre
               className={`text-black font-Founders lg:hidden whitespace-pre-wrap overflow-hidden relative ${
