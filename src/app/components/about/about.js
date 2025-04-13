afa43a4a092aa32af13685b23737490ea7d6aa66
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-13 09:05:23 +0900

    Update about.js

--- src/app/components/about/about.js
+++ src/app/components/about/about.js
@@ -28,7 +28,7 @@ export default function About() {
   }, []);
 
   return (
-    <div className=" lg:flex gap-[4vw] items-start justify-start w-full min-h-[40dvh] py-[6vh] lg:py-[14vh] leading-[1.1] text-[6vw] md:text-[5vw] lg:text-[3.5vw]">
+    <div className=" lg:flex items-start justify-start w-full min-h-[40dvh] py-[6vh] lg:py-[14vh] leading-[1.1] text-[6vw] md:text-[5vw] lg:text-[3.5vw]">
       <h1 className={`${programme.className} text-primaryB mb-4 flex-1 leading-none text-[6vw] mt-[-0.5vh] lg:mt-[-3.5vh]`}>
         Who We Are
       </h1>
