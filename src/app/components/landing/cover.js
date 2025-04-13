165bb185bda7e1b34352199b652ee8f923f57ed9
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-13 23:56:11 +0900

    Update cover.js

--- src/app/components/landing/cover.js
+++ src/app/components/landing/cover.js
@@ -71,10 +71,10 @@ export default function Cover() {
         return (
           <div
             key={index}
-            className="text-center lg:text-left relative inline-block w-full lg:h-[24vw] h-[26vw] lg:h-[3.8vw] leading-[1.1] lg:leading-[1.3] text-[7.5vw] lg:text-[2.5vw]"
+            className="text-center lg:text-left relative inline-block w-full lg:h-[24vw] h-[26vw] lg:h-[3.8vw] leading-[1.1] lg:leading-[1.3] text-[7.5vw] lg:text-[3vw]"
           >
             <pre
-              className={`text-black font-Founders lg:hidden whitespace-pre-wrap overflow-hidden relative ${
+              className={`text-black lg:hidden whitespace-pre-wrap overflow-hidden relative ${
                 index === currentWordIndex ? "after:content-['|'] after:animate-blink" : ""
               }`}
               style={colorStyle}
