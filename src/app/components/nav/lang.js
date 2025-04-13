8aa29e474952df6d3c4da0cae44f3361de5787d2
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-14 00:40:29 +0900

    Update lang.js

--- src/app/components/nav/lang.js
+++ src/app/components/nav/lang.js
@@ -4,8 +4,10 @@ import { programme } from "@/fonts/fonts";
 export default function LanguageToggle({ sectionOn, textColor }){
     const {lang, toggleLang} = useLanguageStore();
     return(
+        
         <button onClick={toggleLang} style={{color:textColor}} className={`${sectionOn === 'cover' ? 'text-primaryW' : 'text-primaryB'} ${programme.className} lg:font-[300] leading-relaxed text-xl font-normal lg:text-[0.9vw] z-[300] w-fit fixed bottom-0 left-0 pl-6 pb-10 lg:pb-0 lg:top-0 lg:left-auto lg:right-0 lg:pt-2 lg:pr-10`}>
-           EN / KR
+          {*/ EN / KR  */}
         </button>
+    }
     );
 }
