2f55eba4fd2a4a5602849d59243bfe41ce919082
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-13 21:22:11 +0900

    Update desc.js

--- src/app/components/about/desc.js
+++ src/app/components/about/desc.js
@@ -29,7 +29,7 @@ function DescItem({ id, title, description, imageUrl }) {
             loading="lazy"
           />
         )}
-        <pre className={`whitespace-pre-wrap ${pxGrotesk.className} px-[0.8em] lg:px-[1.3em] pb-[3vh] ${lang === 'en' ? 'leading-[1.3] lg:leading-[1.38] text-[3vw] md:text-[2.85vw] lg:text-[1.15vw]' : 'leading-[1.8] text-[3.1vw] lg:text-[1.1vw]'} text-primaryB transition-all duration-700 ml-[4px] w-full`}>
+        <pre className={`whitespace-pre-wrap ${pxGrotesk.className} pt:-[0.3em] px-[0.8em] lg:px-[1.3em] pb-[8vh] ${lang === 'en' ? 'leading-[1.3] lg:leading-[1.38] text-[3vw] md:text-[2.85vw] lg:text-[1.15vw]' : 'leading-[1.8] text-[3.1vw] lg:text-[1.1vw]'} text-primaryB transition-all duration-700 ml-[4px] w-full`}>
           {description}
         </pre>
       </div>
