36334c3d3566afe72c48308daa3ab4968c986ffe
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-14 00:09:54 +0900

    Update members.js

--- src/app/components/members.js
+++ src/app/components/members.js
@@ -68,7 +68,7 @@ export default function Members() {
               />
               <div className="md:flex-1 ">
                 <h2 className="leading-[1.1] text-[3.8vw] md:text-[3vw] lg:text-[1vw]">{member[1]}</h2>
-                <pre className={`${pxGrotesk.className} pt-[0.5vh] whitespace-pre-wrap leading-[1.1] text-[3.3vw] md:text-[2.1vw] lg:text-[0.6vw]`}>{member[3]}</pre>
+                <pre className={`${pxGrotesk.className} pt-[0.5vh] whitespace-pre-wrap leading-[1.1] text-[3.3vw] md:text-[2.1vw] lg:text-[0.88vw]`}>{member[3]}</pre>
                 <div className="border-l-[1px] border-current mt-3 pl-3 lg:pl-[0.5vw]">
                   {/* <h3 className="pb-3 md:pb-2 leading-[1.3] text-[3vw] md:text-[1.9vw] lg:text-[0.8vw]">Career</h3> */}
                   <div className="leading-[1.3] text-[3vw] md:text-[1.7vw] lg:text-[0.7vw]">
