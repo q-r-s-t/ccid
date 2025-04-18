3506c71ae199fd1384c5a03f33ebb36c49551bcb
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-18 11:07:32 +0900

    Update desc.js

--- src/app/components/about/desc.js
+++ src/app/components/about/desc.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useLanguageStore } from "../../store/languageStore";
 import { pxGrotesk } from "@/fonts/fonts";
+import { neuehaas } from "@/fonts/fonts";
 
 function DescItem({ id, title, description, imageUrl }) {
   const textStyle =
@@ -13,7 +14,7 @@ function DescItem({ id, title, description, imageUrl }) {
   return (
     <li className="h-auto bg-gradient-to-t from-[rgba(93,0,156,0.2)] via-[rgba(93,0,156,0)] to-[rgba(93,0,156,0)] lg:flex group overflow-hidden transition-all duration-700 ease-out py-2 lg:py-[2vh] lg:px-[5vw]">
       <h3  
-        className={`${lang === 'en' ? textStyle : textStyleKr}  transition-all duration-700 w-full lg:w-[52%] tracking-[0.03] pt-[0.8em] px-[2.1vh] lg:mr-[1vw] mb-[0vh] lg:mb-[2vh]`}
+        className={`${lang === 'en' ? textStyle : textStyleKr} ${neuehaas.className} transition-all duration-700 w-full lg:w-[52%] tracking-[0.03] pt-[0.8em] px-[2.1vh] lg:mr-[1vw] mb-[0vh] lg:mb-[2vh]`}
       >
         {title}
       </h3>
