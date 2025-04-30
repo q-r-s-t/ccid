9ec6dc34ac6f37646c971c6c26235e4084bb9372
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-04-30 16:16:50 +0900

    Update cover.js

--- src/app/components/landing/cover.js
+++ src/app/components/landing/cover.js
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { neuehaas } from "@/fonts/fonts";
@@ -57,16 +58,15 @@ export default function Cover() {
   }, [currentWordIndex, mainText]);
 
   return (
-    <div className="flex flex-col w-full h-full lg:pt-[38dvh] pt-[28vh] px-6 lg:px-10">
+    <div className={`flex flex-col w-full h-full lg:pt-[38dvh] pt-[28vh] px-6 lg:px-10`}>
       {typedWords.map((word, index) => {
-        const isActive = index === currentWordIndex;
-        const hasAppeared = index <= currentWordIndex;
-
-        const colorStyle = hasAppeared
-          ? { color: "#fff", transition: "color 3.8s ease-in-out" }
+        const shouldApplyColor = index <= currentWordIndex;
+        const colorStyle = shouldApplyColor
+          ? {
+              color: "#fff",
+              transition: "color 3.8s ease-in-out",
+            }
           : {};
-
-        const animationClass = isActive ? "animate-fadeInUp" : "";
 
         return (
           <div
@@ -74,21 +74,22 @@ export default function Cover() {
             className="text-center lg:text-left relative inline-block w-full lg:h-[24vw] h-[26vw] lg:h-[3.8vw] leading-[1.1] lg:leading-[1.3] text-[7.5vw] lg:text-[3vw]"
           >
             <pre
-              className={`${neuehaas.className} text-black tracking-[1px] lg:hidden whitespace-pre-wrap overflow-hidden relative ${animationClass} ${
-                isActive ? "after:content-['|'] after:animate-blink" : ""
+              className={`${neuehaas.className} text-black tracking-[1px] lg:hidden whitespace-pre-wrap overflow-hidden relative ${
+                index === currentWordIndex ? 'blinking-cursor' : ''
               }`}
               style={colorStyle}
             >
               {word}
             </pre>
             <p
-              className={`${neuehaas.className} text-black tracking-[-1px] hidden lg:block overflow-hidden relative ${animationClass} ${
-                isActive ? "after:content-['|'] after:animate-blink" : ""
+              className={`${neuehaas.className} text-black tracking-[-1px] hidden lg:block overflow-hidden relative ${
+                index === currentWordIndex ? 'blinking-cursor' : ''
               }`}
               style={colorStyle}
             >
               {word}
             </p>
+
           </div>
         );
       })}
