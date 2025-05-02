851a47db11f5934ec34121dc4043595d12160912
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-05-03 00:37:00 +0900

    Update about.js

--- src/app/components/about/about.js
+++ src/app/components/about/about.js
@@ -29,10 +29,6 @@ export default function About() {
 
   return (
     <div className=" lg:flex gap-[4vw] items-start justify-start w-full min-h-[40dvh] py-[6vh] lg:py-[14vh] leading-[1.1] text-[6vw] md:text-[5vw] lg:text-[3.5vw]">
-      <h1 className={`${programme.className} text-primaryB mb-4 flex-1 leading-none text-[6vw] mt-[-0.5vh] lg:mt-[-3.5vh]`}>
-        Who We Are
-      </h1>
-
       <div className={`${pxGrotesk.className} text-primaryB flex-1`}>
         {aboutInfo.length > 0 ? (
           <pre className={`${pxGrotesk.className} ${lang === 'en' ? 'leading-[1.55] text-[3vw] md:text-[2.85vw] lg:text-[1.15vw]' : 'leading-[1.8] text-[3.1vw] lg:text-[1.1vw]'} whitespace-pre-wrap mb-[4vh]`}>{lang === 'en' ? aboutInfo[0][0] : aboutInfo[0][1]}</pre>
