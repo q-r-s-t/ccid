62a8a7a1b086f304af94e1aee576db05f5234f87
q-r-s-t <157675141+q-r-s-t@users.noreply.github.com>
2025-05-02 11:52:54 +0900

    Update works.js

--- src/app/components/works/works.js
+++ src/app/components/works/works.js
@@ -29,84 +29,85 @@ export default function Works({ textColor }) {
   }, []);
 
   return (
-    <div className=" lg:flex gap-[4vw] items-start justify-start w-full min-h-[40dvh] py-[6vh] lg:py-[14vh] leading-[1.1] text-[6vw] md:text-[5vw] lg:text-[3.5vw]">
-      <h1 className={`${programme.className} text-primaryB mb-4 flex-1 leading-none text-[6vw] mt-[-0.5vh] lg:mt-[-3.5vh]`}>
-        Selected Projects
-      </h1>
-    </div>
-    <div
-      className={`text-primaryB pt-[12%] pb-[80%] ${lang === 'kr' ? 'lg:pt-[4dvh]' : 'lg:pt-[8%]'} lg:pb-[10%] lg:px-[5vw] w-full h-full font-[400]`}
-    >
+    <div className="w-full">
+      <div className="lg:flex gap-[4vw] items-start justify-start w-full min-h-[40dvh] py-[6vh] lg:py-[14vh] leading-[1.1] text-[6vw] md:text-[5vw] lg:text-[3.5vw]">
+        <h1 className={`${neuehaas.className} text-primaryB mb-4 flex-1 leading-none text-[6vw] mt-[-0.5vh] lg:mt-[-3.5vh]`}>
+          Selected Projects
+        </h1>
+      </div>
+      <div
+        className={`text-primaryB pt-[12%] pb-[80%] ${lang === 'kr' ? 'lg:pt-[4dvh]' : 'lg:pt-[8%]'} lg:pb-[10%] lg:px-[5vw] w-full h-full font-[400]`}
+      >
         {worksInfo.length > 0 ? (
-        worksInfo.map((row, i) =>
-          i % 3 === 0
-            ? row.map((col, j) => {
-                const year = worksInfo[i][j];
-                const international = lang === 'en' ? worksInfo[i + 1]?.[j] : worksInfo[i + 2]?.[j];
-  
-                // Check if both values are not empty
-                if ((year !== undefined && year !== "") || (international !== undefined && international !== "")) {
-                  return (
-                    <div key={`${i}-${j}`} className="relative">
-                      <div
-                        className={` ${lang === 'en' ? 'leading-[1.4] mb-[0.5vh]' : 'lg:leading-[1.5] leading-[1.1] mb-[1.1vh] lg:mb-[0.3vh]'} relative flex text-[2.8vw] md:text-[2.4vw] lg:text-[1vw]  
-                        ${j === 0 ? `text-primaryC lg:pl-10 lg:h-[7dvh] lg:absolute lg:top-0 pl-6 lg:left-0 lg:w-[20vw]` : ""}`}               
-                      >
-                        {/* 년도 */}
+          worksInfo.map((row, i) =>
+            i % 3 === 0
+              ? row.map((col, j) => {
+                  const year = worksInfo[i][j];
+                  const international = lang === 'en' ? worksInfo[i + 1]?.[j] : worksInfo[i + 2]?.[j];
+    
+                  // Check if both values are not empty
+                  if ((year !== undefined && year !== "") || (international !== undefined && international !== "")) {
+                    return (
+                      <div key={`${i}-${j}`} className="relative">
                         <div
-                          className={`${j === 1 ? `${lang === 'en' ? 'lg:mt-[0vh]' : 'lg:mt-[2vh]'}` : ''} ${
-                            j === 0 ? "hidden" : ""
-                          }  ${pxGrotesk.className} flex-[0.35] lg:pl-[22.5%] lg:flex-[0.1] pl-6 lg:pl-12 tracking-1.8`}
+                          className={` ${lang === 'en' ? 'leading-[1.4] mb-[0.5vh]' : 'lg:leading-[1.5] leading-[1.1] mb-[1.1vh] lg:mb-[0.3vh]'} relative flex text-[2.8vw] md:text-[2.4vw] lg:text-[1vw]  
+                          ${j === 0 ? `text-primaryC lg:pl-10 lg:h-[7dvh] lg:absolute lg:top-0 pl-6 lg:left-0 lg:w-[20vw]` : ""}`}               
                         >
-                          {year}
+                          <div
+                            className={`${j === 1 ? `${lang === 'en' ? 'lg:mt-[0vh]' : 'lg:mt-[2vh]'}` : ''} ${
+                              j === 0 ? "hidden" : ""
+                            }  ${pxGrotesk.className} flex-[0.35] lg:pl-[22.5%] lg:flex-[0.1] pl-6 lg:pl-12 tracking-1.8`}
+                          >
+                            {year}
+                          </div>
+    
+                          <p
+                            className={`indent-except-first flex-1 pr-6 
+                              ${j === 0 
+                                ? `text-primaryC pretendardR lg:block flex items-end text-[3.3vw] md:text-[2.6vw] lg:text-[1.03vw] leading-[0.88] lg:leading-[1.1] pt-[8dvh] lg:pt-[0dvh] pb-[1dvh] lg:pb-[0.7dvh] ${lang === 'en' ? 'lg:mt-[0vh]' : 'lg:mt-[2vh]'}`
+                                : ""} 
+                              ${j === 1 ? `${lang === 'en' ? 'lg:mt-[0vh]' : 'lg:mt-[2vh]'}` : ``}`}
+                          >
+                            {international && international.includes(':') 
+                              ? international.split(':').map((part, index) => {
+                                  if (index === 0) {
+                                    return (
+                                      <span key={index}>
+                                        {part.split('').map((char, charIndex) => (
+                                          <span 
+                                            key={charIndex} 
+                                            className="char-reveal"
+                                            style={{ 
+                                              display: 'inline-block',
+                                              width: char === " " ? '0.3em' : 'auto',
+                                              animationDelay: `${charIndex * 800}ms`
+                                            }}
+                                          >
+                                            {char}
+                                          </span>
+                                        ))}
+                                      </span>
+                                    );
+                                  } else {
+                                    return <span key={index}>:{part}</span>;
+                                  }
+                                })
+                              : international
+                            }
+                          </p>
+    
                         </div>
-  
-                        <p
-                          className={`indent-except-first flex-1 pr-6 
-                            ${j === 0 
-                              ? `text-primaryC pretendardR lg:block flex items-end text-[3.3vw] md:text-[2.6vw] lg:text-[1.03vw] leading-[0.88] lg:leading-[1.1] pt-[8dvh] lg:pt-[0dvh] pb-[1dvh] lg:pb-[0.7dvh] ${lang === 'en' ? 'lg:mt-[0vh]' : 'lg:mt-[2vh]'}`
-                              : ""} 
-                            ${j === 1 ? `${lang === 'en' ? 'lg:mt-[0vh]' : 'lg:mt-[2vh]'}` : ``}`}
-                        >
-                          {international && international.includes(':') 
-                            ? international.split(':').map((part, index) => {
-                                if (index === 0) {
-                                  return (
-                                    <span key={index}>
-                                      {part.split('').map((char, charIndex) => (
-                                        <span 
-                                          key={charIndex} 
-                                          className="char-reveal"
-                                          style={{ 
-                                            display: 'inline-block',
-                                            width: char === " " ? '0.3em' : 'auto',
-                                            animationDelay: `${charIndex * 800}ms`
-                                          }}
-                                        >
-                                          {char}
-                                        </span>
-                                      ))}
-                                    </span>
-                                  );
-                                } else {
-                                  return <span key={index}>:{part}</span>;
-                                }
-                              })
-                            : international
-                          }
-                        </p>
-
                       </div>
-                    </div>
-                  );
-                }
-                return null; // Don't render anything if both are empty
-              })
-            : null
-        )
-      ) : (
-        <div>Loading...</div>
-      )}
+                    );
+                  }
+                  return null; // Don't render anything if both are empty
+                })
+              : null
+          )
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
     </div>
   );
   
